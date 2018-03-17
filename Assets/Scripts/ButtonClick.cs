0690612f34ac0d5e58969d4384e187b546d1500e
David Knolls <Therobodavo@gmail.com>
2018-03-17 17:30:20 -0400

    based on data

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -4,46 +4,36 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+//Button Click Class
+//Stores methods trigged by button clicks
+
 public class ButtonClick : MonoBehaviour
 {
-    public GameObject selection;
-    GameObject selectedNum;
-    GameObject selectedType;
-    private void Deactivate() 
-    {
-        selectedNum.GetComponent<Button>().interactable = false;
-    }
-    private void Disable() 
-    {
-        selectedType.SetActive(false);
-    }
+    //Clears selected num and type
     private void ClearSelection() 
     {
-        selection.transform.GetChild(1).gameObject.SetActive(false);
-        selection.transform.GetChild(2).gameObject.SetActive(false);
+        this.gameObject.GetComponent<GameEngine>().playerNumSelected = null;
+        this.gameObject.GetComponent<GameEngine>().playerTypeSelected = null;
     }
+
+    //Makes turn final, clears selected and updates active choices
     public void LockIn()
     {
-        Deactivate();
-        Disable();
+        this.gameObject.GetComponent<GameEngine>().playerNumActive[this.gameObject.GetComponent<GameEngine>().playerNumSelected.transform.GetSiblingIndex() - 1] = false;
+        this.gameObject.GetComponent<GameEngine>().playerTypeActive[this.gameObject.GetComponent<GameEngine>().playerTypeSelected.transform.GetSiblingIndex() - 1] = false;
         ClearSelection();
     }
+
+    //Sets selected num to the one clicked
     public void SelectNum() 
     {
-        selectedNum = EventSystem.current.currentSelectedGameObject;
-        selection.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = selectedNum.transform.GetChild(0).GetComponent<Text>().text;
-        if(!selection.transform.GetChild(1).gameObject.activeSelf) 
-        {
-            selection.transform.GetChild(1).gameObject.SetActive(true);
-        }
+        this.gameObject.GetComponent<GameEngine>().playerNumSelected = EventSystem.current.currentSelectedGameObject;
+        
     }
+
+    //Sets selected type to the one clicked
     public void SelectType() 
     {
-        selectedType = EventSystem.current.currentSelectedGameObject;
-        selection.transform.GetChild(2).GetChild(0).GetComponent<Text>().text = selectedType.transform.GetChild(0).GetComponent<Text>().text;
-        if(!selection.transform.GetChild(2).gameObject.activeSelf) 
-        {
-            selection.transform.GetChild(2).gameObject.SetActive(true);
-        }
+        this.gameObject.GetComponent<GameEngine>().playerTypeSelected = EventSystem.current.currentSelectedGameObject;
     }
 }
---
+++ Assets/Scripts/GameEngine.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Game Engine Class
+// Runs Start and Update to keep track of player data
+
+public class GameEngine : MonoBehaviour {
+    
+    //Number/Type button status. True = active, False = deactive
+    public bool[] playerNumActive = new bool[9];
+    public bool[] playerTypeActive = new bool[9];
+
+    //Current selected Num and Type
+    public GameObject playerTypeSelected = null;
+    public GameObject playerNumSelected = null;
+
+    //Scene Objects
+    public GameObject selector;
+    public GameObject numbers;
+    public GameObject types;
+    public GameObject lockInBtn;
+
+	void Start ()
+    {
+        //Sets all buttons and types to active
+		for(int i = 0; i < playerNumActive.Length; i++) 
+        {
+            playerNumActive[i] = true;
+            playerTypeActive[i] = true;
+        }
+	}
+	
+	void Update ()
+    {
+        //Shows states of numbers and types
+		for(int i = 0; i < playerNumActive.Length; i++) 
+        {
+            if(!playerNumActive[i] && numbers.transform.GetChild(i).gameObject.activeSelf)
+            {
+                numbers.transform.GetChild(i + 1).gameObject.GetComponent<Button>().interactable = false;   
+            }
+            if(!playerTypeActive[i] && types.transform.GetChild(i).gameObject.activeSelf) 
+            {
+                types.transform.GetChild(i + 1).gameObject.SetActive(false);  
+            }
+        }
+
+        //Shows current selections
+        if(playerNumSelected != null)
+        {
+            selector.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = playerNumSelected.transform.GetChild(0).GetComponent<Text>().text;
+            selector.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            selector.transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+        if(playerTypeSelected != null) 
+        {
+            selector.transform.GetChild(2).GetChild(0).GetComponent<Text>().text = playerTypeSelected.transform.GetChild(0).GetComponent<Text>().text;
+            selector.transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else
+        {
+            selector.transform.GetChild(2).gameObject.SetActive(false);
+        }
+
+        //Shows state of lock in button
+        if(playerNumSelected == null || playerTypeSelected == null)
+        {
+            lockInBtn.GetComponent<Button>().interactable = false;
+        }
+        else if(!lockInBtn.GetComponent<Button>().interactable)
+        {
+            lockInBtn.GetComponent<Button>().interactable = true;
+        }
+	}
+}
