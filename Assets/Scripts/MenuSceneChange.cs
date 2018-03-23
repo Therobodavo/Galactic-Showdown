bfb76715b74696be363338a8cd867858e55a49e2
Tristan Marshall <tam8217@g.rit.edu>
2018-03-23 00:47:14 -0400

    Added Menu, changed numpad orientation

---
+++ Assets/Scripts/MenuSceneChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneChange : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+    public GameObject mainPanel;
+
+    public GameObject helpPanel;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowHelp()
+    {
+        mainPanel.SetActive(false);
+        helpPanel.SetActive(true);
+    }
+
+    public void ShowMain()
+    {
+        mainPanel.SetActive(true);
+        helpPanel.SetActive(false);
+    }
+}
