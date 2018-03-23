4cbdb9ec8fb9f51fa71f371dfaa95837302e75b4
Matthew Oelbaum (RIT Student) <mro5772@ad.rit.edu>
2018-03-22 22:19:56 -0400

    Adding Comparison

---
+++ Assets/Scripts/Animate.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate : MonoBehaviour {
+
+    //Animation class
+
+    //attributes
+    SpriteRenderer Rend; //render
+    public Sprite[] Number; // holds each sprite
+    public Sprite[] noNumber; // holds each sprite
+ 
+    Sprite[] animations; // holds each sprite
+    public float Timer; // timer to change sprite
+    public int AniTime; //specfic timer that controls frames
+    public float increase;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Timer = 0;
+        AniTime = 0;
+        Rend = (SpriteRenderer)GetComponent("SpriteRenderer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Timer += increase;
+        animations = Number;
+        
+
+        AniTime = (int)Mathf.Round(Timer);
+
+        if (AniTime >= animations.Length)
+        {
+            Timer = 0;
+        }
+
+        if (AniTime < animations.Length)
+        {
+            Rend.sprite = animations[AniTime];
+        }
+    }
+}
---
+++ Assets/Scripts/CompareResults.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompareResults : MonoBehaviour {
+
+    public GameObject playerTypeSelected = null;
+    public GameObject playerNumSelected = null;
+
+    //Player 1 attributes
+    public int p1Type; // 1- highest, 2 lowest, 3 middle
+    public int p1Num;
+    //Player 2 attributes
+    public int p2Type;
+  public int p2Num;
+    //Winning Type
+    public int winType;
+    //Winnung Player
+    public int Winner; // 1- player1 wins, 2- player2 wins, 3- Draw
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //compare will start once the play selects both type and num
+        if(this.gameObject.GetComponent<GameEngine>().playerTypeSelected != null && this.gameObject.GetComponent<GameEngine>().playerNumSelected != null)
+        {
+            checkInfo();
+            compareInfo();
+        }
+      
+       
+       
+	}
+
+    public void checkInfo()
+    {
+       //cheking what variables player 1 choose
+        playerTypeSelected = this.gameObject.GetComponent<GameEngine>().playerTypeSelected;
+        playerNumSelected = this.gameObject.GetComponent<GameEngine>().playerNumSelected;
+        //storing chosen values
+        p1Type = playerTypeSelected.GetComponent<objectValue>().value;
+        p1Num = playerNumSelected.GetComponent<objectValue>().value;
+
+    }
+    public void compareInfo()
+    {
+        //Type 1 wins
+        if (p1Type + p2Type == 2 || p1Type + p2Type == 3) 
+        {
+            winType = 1;
+        }
+        //Type 2 wins
+        if ((p1Type + p2Type == 4 && p1Type == 2) || p1Type + p2Type == 5) 
+        {
+            winType = 2;
+        }
+        //Type 3 wins
+        if ((p1Type + p2Type == 4 && p1Type != 2) || p1Type + p2Type == 6) 
+        {
+            winType = 3;
+        }
+
+        //Highest number wins
+        if (winType == 1)
+        {
+            if (p1Num > p2Num)
+                Winner = 1;
+            else if (p1Num < p2Num)
+                Winner = 2;
+            else if (p1Num == p2Num)
+                Winner = 3;
+        }
+        //Lowest Number wins
+        if (winType == 2)
+        {
+            if (p1Num > p2Num)
+                Winner = 2;
+            else if (p1Num < p2Num)
+                Winner = 1;
+            else if (p1Num == p2Num)
+                Winner = 3;
+        }
+        //Number closest to 5 wins
+        if (winType == 3)
+        {
+            if (Mathf.Abs(p1Num - 5) > Mathf.Abs(p2Num - 5))
+                Winner = 2;
+            else if (Mathf.Abs(p1Num - 5) < Mathf.Abs(p2Num - 5))
+                Winner = 1;
+            else if (Mathf.Abs(p1Num - 5) == Mathf.Abs(p2Num - 5))
+                Winner = 3;
+        }
+    }
+}
