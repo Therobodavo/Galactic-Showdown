da297785a1cd466ce8fadbdd1093a7653ceb2d5b
mro5772 <mro5772@.g.rit.edu>
2018-03-25 19:35:52 -0700

    I'm afraid of commitments

--- Assets/Scripts/CompareShow.cs
+++ Assets/Scripts/CompareShow.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+
 
 //Inheriting from CompareResults to have access to methods
 public class CompareShow : MonoBehaviour
@@ -15,15 +18,38 @@ public class CompareShow : MonoBehaviour
     public Text p2WinText;
     string winnerNum;
     string winnerType;
-    
-	// Use this for initialization
-	void Start()
+
+    float timer;
+   public  float timerDelay;
+    float stage;
+    float Winner; // 1- player1, 2- player 2, 3- tie
+
+    public Sprite[] NumSprites;
+    public Sprite[] TypeSprites;
+  public   GameObject P1;
+  public  GameObject P2;
+    GameObject Win;
+
+    // Use this for initialization
+    void Start()
     {
+        timer = 0;
+        stage = 0;
+
         //Getting the players script to access data
         pd = GameObject.Find("PlayersData");
 
+
+        //accessing player game objects
+        P1 = GameObject.FindGameObjectWithTag("P1");
+        P1.SetActive(false);
+        P2 = GameObject.FindGameObjectWithTag("P2");
+        P2.SetActive(false);
+        Win = GameObject.FindGameObjectWithTag("Winner");
+        Win.SetActive(false);
+
         //Gets the type and number winner
-        winnerType= CompareType(pd.GetComponent<Players>().player1.typeSelected, pd.GetComponent<Players>().player2.typeSelected);      
+        winnerType = CompareType(pd.GetComponent<Players>().player1.typeSelected, pd.GetComponent<Players>().player2.typeSelected);      
         winnerNum = CompareNum(pd.GetComponent<Players>().player1.numSelected, pd.GetComponent<Players>().player2.numSelected, winnerType);
 
         //Debug shows the turn choices
@@ -35,28 +61,101 @@ public class CompareShow : MonoBehaviour
         //Checks who won
         if(pd.GetComponent<Players>().player1.numSelected == winnerNum && pd.GetComponent<Players>().player2.numSelected == winnerNum)
         {
-            winText.text = "Neither player wins, it's a draw!";
+            Winner = 3;
+          //  winText.text = "Neither player wins, it's a draw!";
         }
         else if(pd.GetComponent<Players>().player1.numSelected == winnerNum)
         {
-            winText.text = "Player 1 wins!";
+            Winner = 1;
+           // winText.text = "Player 1 wins!";
             pd.GetComponent<Players>().player1.roundsWon++;
         }
         else if(pd.GetComponent<Players>().player2.numSelected == winnerNum)
         {
-            winText.text = "Player 2 wins!";
+            Winner = 2;
+           // winText.text = "Player 2 wins!";
             pd.GetComponent<Players>().player2.roundsWon++;
         }
 
-        //Displaying how many wins each player has
-        p1WinText.text = "Player 1 wins: " + pd.GetComponent<Players>().player1.roundsWon;
-        p2WinText.text = "Player 2 wins: " + pd.GetComponent<Players>().player2.roundsWon;
+      
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
+        if(timer > timerDelay * Time.deltaTime)
+        {
+            timer = 0;
+            stage++;
+        }
 
+        if(stage == 0)
+        {
+            winText.text = "Player Actions";
+            timer+= 1 * Time.deltaTime;
+        }
+        if (stage == 1)
+        {
+            P1.SetActive(true);
+            P1.GetComponent<SpriteRenderer>().sprite = TypeSprites[CheckValue(pd.GetComponent<Players>().player1.typeSelected)];
+            timer += 2 * Time.deltaTime;
+        }
+        if (stage == 2)
+        {
+            P2.SetActive(true);
+            P2.GetComponent<SpriteRenderer>().sprite = TypeSprites[CheckValue(pd.GetComponent<Players>().player2.typeSelected)];
+            timer += 2 * Time.deltaTime;
+        }
+        if (stage == 3)
+        {
+            P1.SetActive(false);
+            P2.SetActive(false);
+            winText.text = "Winning Action";
+            Win.SetActive(true);
+            Win.GetComponent<SpriteRenderer>().sprite = TypeSprites[CheckValue(winnerType)];
+            timer += 1 * Time.deltaTime;
+        }
+        if (stage == 5)
+        {
+            winText.text = "Number of Ships Sent";
+            timer += 2 * Time.deltaTime;
+        }
+        if (stage == 6)
+        {
+            P1.SetActive(true);
+            P1.GetComponent<SpriteRenderer>().sprite = NumSprites[Int32.Parse((pd.GetComponent<Players>().player1.numSelected))];
+            timer += 2 * Time.deltaTime;
+        }
+        if (stage == 7)
+        {
+            P2.SetActive(true);
+            P2.GetComponent<SpriteRenderer>().sprite = NumSprites[Int32.Parse((pd.GetComponent<Players>().player2.numSelected))];
+            timer += 2 * Time.deltaTime;
+        }
+        if (stage == 8)
+        {
+            P1.SetActive(false);
+            P2.SetActive(false);
+            Win.SetActive(false);
+
+            if (Winner == 1)
+                winText.text = "Player 1 Wins!!!";
+            if (Winner == 2)
+                winText.text = "Player 2 Wins!!!";
+            if (Winner == 3)
+                winText.text = "Neither player wins, it's a draw!";
+
+            //Displaying how many wins each player has
+            p1WinText.text = "Player 1 wins: " + pd.GetComponent<Players>().player1.roundsWon;
+            p2WinText.text = "Player 2 wins: " + pd.GetComponent<Players>().player2.roundsWon;
+            timer += 1 * Time.deltaTime;
+
+        }
+        if(stage == 9)
+        {
+            SwitchScene();
+        }
+        
     }
 
     //Compares 2 types
@@ -127,7 +226,16 @@ public class CompareShow : MonoBehaviour
          }
          return winner;
     }
+    int CheckValue(string Type)
+    {
+        if (Type == "Type1")
+            return 0;
+       else if (Type == "Type2")
+            return 1;
+        else 
+            return 2;
 
+    }
     //Switching back to the main scene
     public void SwitchScene()
     {
